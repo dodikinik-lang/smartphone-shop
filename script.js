748302b395632b4ba72331988a9b4e2651f59638
dodikinik-lang <dodikinik@gmail.com>
2025-11-13 13:24:28 +1000

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,29 @@ const tg = window.Telegram.WebApp;
 tg.ready(); tg.expand();
 
 const phones = [
-    {id:1,fullName:"Samsung Galaxy A17",variant:"4Gb/128Gb чёрный EAC",img:"https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-a17-5g-1.jpg",img2:"https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-a17-5g-2.jpg",oldPrice:15999,newPrice:14999,specs:["6.5\" PLS","Helio G85","50MP","5000 mAh"],brand:"Samsung"},
-    {id:2,fullName:"Samsung Galaxy A56",variant:"8Gb/256Gb чёрный EAC",img:"https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-a56-1.jpg",img2:"https://fdn2.gsmarena.com/vv/pics/samsung/samsung-galaxy-a56-2.jpg",oldPrice:39999,newPrice:35499,specs:["6.7\" AMOLED 120Hz","Exynos 1480","50MP","5000 mAh"],brand:"Samsung"},
-    {id:3,fullName:"Honor 400 Pro",variant:"12Gb/256Gb чёрный EAC",img:"https://fdn2.gsmarena.com/vv/pics/honor/honor-400-pro-1.jpg",img2:"https://fdn2.gsmarena.com/vv/pics/honor/honor-400-pro-2.jpg",oldPrice:56999,newPrice:51999,specs:["6.8\" OLED 120Hz","Snapdragon 8 Gen 3","200MP","5500 mAh"],brand:"Honor"},
-    {id:4,fullName:"Huawei nova 14 Pro",variant:"12Gb/512Gb чёрный EAC",img:"https://fdn2.gsmarena.com/vv/pics/huawei/huawei-nova-14-pro-1.jpg",img2:"https://fdn2.gsmarena.com/vv/pics/huawei/huawei-nova-14-pro-2.jpg",oldPrice:46999,newPrice:44999,specs:["6.78\" OLED 120Hz","Kirin 9000","50MP переменная","4500 mAh"],brand:"Huawei"}
+    {id:1,fullName:"Samsung Galaxy A17",variant:"4Gb/128Gb чёрный EAC",img:"https://img.dns-shop.ru/img/goods/1/1234567890_1.jpg",img2:"https://img.dns-shop.ru/img/goods/1/1234567890_2.jpg",oldPrice:15999,newPrice:14999,specs:["6.5\" PLS","Helio G85","50MP","5000 mAh"],brand:"Samsung"},
+    {id:2,fullName:"Samsung Galaxy A17",variant:"8Gb/256Gb чёрный EAC",img:"https://img.dns-shop.ru/img/goods/1/1234567891_1.jpg",img2:"https://img.dns-shop.ru/img/goods/1/1234567891_2.jpg",oldPrice:20999,newPrice:19999,specs:["6.5\" PLS","Helio G85","50MP","5000 mAh"],brand:"Samsung"},
+    {id:3,fullName:"Samsung Galaxy A26",variant:"6Gb/128Gb синий EAC",img:"https://img.dns-shop.ru/img/goods/1/1234567892_1.jpg",img2:"https://img.dns-shop.ru/img/goods/1/1234567892_2.jpg",oldPrice:22999,newPrice:21999,specs:["6.6\" Super AMOLED","Exynos 1330","48MP","5000 mAh"],brand:"Samsung"},
+    {id:4,fullName:"Samsung Galaxy A26",variant:"8Gb/256Gb чёрный EAC",img:"https://img.dns-shop.ru/img/goods/1/1234567893_1.jpg",img2:"https://img.dns-shop.ru/img/goods/1/1234567893_2.jpg",oldPrice:25999,newPrice:24999,specs:["6.6\" Super AMOLED","Exynos 1330","48MP","5000 mAh"],brand:"Samsung"},
+    {id:5,fullName:"Samsung Galaxy A36",variant:"8Gb/128Gb белый EAC",img:"https://img.dns-shop.ru/img/goods/1/1234567894_1.jpg",img2:"https://img.dns-shop.ru/img/goods/1/1234567894_2.jpg",oldPrice:28999,newPrice:27499,specs:["6.7\" Super AMOLED","Exynos 1380","50MP OIS","5000 mAh"],brand:"Samsung"},
+    {id:6,fullName:"Samsung Galaxy A36",variant:"8Gb/256Gb чёрный EAC",img:"https://img.dns-shop.ru/img/goods/1/1234567895_1.jpg",img2:"https://img.dns-shop.ru/img/goods/1/1234567895_2.jpg",oldPrice:32999,newPrice:31499,specs:["6.7\" Super AMOLED","Exynos 1380","50MP OIS","5000 mAh"],brand:"Samsung"},
+    {id:7,fullName:"Samsung Galaxy A56",variant:"8Gb/128Gb зелёный EAC",img:"https://img.dns-shop.ru/img/goods/1/1234567896_1.jpg",img2:"https://img.dns-shop.ru/img/goods/1/1234567896_2.jpg",oldPrice:35999,newPrice:34499,specs:["6.7\" AMOLED 120Hz","Exynos 1480","50MP","5000 mAh"],brand:"Samsung"},
+    {id:8,fullName:"Samsung Galaxy A56",variant:"8Gb/256Gb чёрный EAC",img:"https://img.dns-shop.ru/img/goods/1/1234567897_1.jpg",img2:"https://img.dns-shop.ru/img/goods/1/1234567897_2.jpg",oldPrice:39999,newPrice:35499,specs:["6.7\" AMOLED 120Hz","Exynos 1480","50MP","5000 mAh"],brand:"Samsung"},
+    {id:9,fullName:"Honor X6C",variant:"6Gb/128Gb чёрный EAC",img:"https://img.dns-shop.ru/img/goods/1/1234567898_1.jpg",img2:"https://img.dns-shop.ru/img/goods/1/1234567898_2.jpg",oldPrice:9999,newPrice:9499,specs:["6.5\" IPS","Helio G85","50MP","5000 mAh"],brand:"Honor"},
+    {id:10,fullName:"Honor X6C",variant:"6Gb/256Gb синий EAC",img:"https://img.dns-shop.ru/img/goods/1/1234567899_1.jpg",img2:"https://img.dns-shop.ru/img/goods/1/1234567899_2.jpg",oldPrice:11999,newPrice:11499,specs:["6.5\" IPS","Helio G85","50MP","5000 mAh"],brand:"Honor"},
+    {id:11,fullName:"Honor X7C",variant:"6Gb/128Gb белый EAC",img:"https://img.dns-shop.ru/img/goods/1/1234567900_1.jpg",img2:"https://img.dns-shop.ru/img/goods/1/1234567900_2.jpg",oldPrice:13999,newPrice:12499,specs:["6.6\" AMOLED","Snapdragon 680","64MP","5000 mAh"],brand:"Honor"},
+    {id:12,fullName:"Honor X7C",variant:"8Gb/256Gb чёрный EAC",img:"https://img.dns-shop.ru/img/goods/1/1234567901_1.jpg",img2:"https://img.dns-shop.ru/img/goods/1/1234567901_2.jpg",oldPrice:16999,newPrice:15499,specs:["6.6\" AMOLED","Snapdragon 680","64MP","5000 mAh"],brand:"Honor"},
+    {id:13,fullName:"Honor X8C",variant:"8Gb/128Gb серебристый EAC",img:"https://img.dns-shop.ru/img/goods/1/1234567902_1.jpg",img2:"https://img.dns-shop.ru/img/goods/1/1234567902_2.jpg",oldPrice:17999,newPrice:16499,specs:["6.7\" AMOLED 120Hz","Snapdragon 7 Gen 1","108MP","4500 mAh"],brand:"Honor"},
+    {id:14,fullName:"Honor X8C",variant:"8Gb/256Gb чёрный EAC",img:"https://img.dns-shop.ru/img/goods/1/1234567903_1.jpg",img2:"https://img.dns-shop.ru/img/goods/1/1234567903_2.jpg",oldPrice:19999,newPrice:18499,specs:["6.7\" AMOLED 120Hz","Snapdragon 7 Gen 1","108MP","4500 mAh"],brand:"Honor"},
+    {id:15,fullName:"Honor X9C",variant:"8Gb/256Gb золотой EAC",img:"https://img.dns-shop.ru/img/goods/1/1234567904_1.jpg",img2:"https://img.dns-shop.ru/img/goods/1/1234567904_2.jpg",oldPrice:24999,newPrice:23999,specs:["6.78\" OLED 120Hz","Snapdragon 8 Gen 2","200MP","5000 mAh"],brand:"Honor"},
+    {id:16,fullName:"Honor 400 Lite",variant:"8Gb/256Gb чёрный EAC",img:"https://img.dns-shop.ru/img/goods/1/1234567905_1.jpg",img2:"https://img.dns-shop.ru/img/goods/1/1234567905_2.jpg",oldPrice:24999,newPrice:21499,specs:["6.7\" AMOLED","Dimensity 6020","108MP","5000 mAh"],brand:"Honor"},
+    {id:17,fullName:"Honor 400",variant:"8Gb/256Gb белый EAC",img:"https://img.dns-shop.ru/img/goods/1/1234567906_1.jpg",img2:"https://img.dns-shop.ru/img/goods/1/1234567906_2.jpg",oldPrice:36999,newPrice:33999,specs:["6.78\" OLED","Snapdragon 8 Gen 3","50MP","5300 mAh"],brand:"Honor"},
+    {id:18,fullName:"Honor 400 Pro",variant:"12Gb/256Gb чёрный EAC",img:"https://img.dns-shop.ru/img/goods/1/1234567907_1.jpg",img2:"https://img.dns-shop.ru/img/goods/1/1234567907_2.jpg",oldPrice:56999,newPrice:51999,specs:["6.8\" OLED 120Hz","Snapdragon 8 Gen 3","200MP","5500 mAh"],brand:"Honor"},
+    {id:19,fullName:"Huawei nova Y63",variant:"4Gb/128Gb чёрный EAC",img:"https://img.dns-shop.ru/img/goods/1/1234567908_1.jpg",img2:"https://img.dns-shop.ru/img/goods/1/1234567908_2.jpg",oldPrice:9999,newPrice:8499,specs:["6.52\" IPS","Snapdragon 680","50MP","5000 mAh"],brand:"Huawei"},
+    {id:20,fullName:"Huawei nova Y73",variant:"8Gb/256Gb синий EAC",img:"https://img.dns-shop.ru/img/goods/1/1234567909_1.jpg",img2:"https://img.dns-shop.ru/img/goods/1/1234567909_2.jpg",oldPrice:14999,newPrice:13499,specs:["6.75\" IPS 90Hz","Snapdragon 680","50MP","5000 mAh"],brand:"Huawei"},
+    {id:21,fullName:"Huawei nova 14",variant:"12Gb/256Gb розовый EAC",img:"https://img.dns-shop.ru/img/goods/1/1234567910_1.jpg",img2:"https://img.dns-shop.ru/img/goods/1/1234567910_2.jpg",oldPrice:33999,newPrice:21999,specs:["6.7\" OLED","Kirin 8000","60MP","4500 mAh"],brand:"Huawei"},
+    {id:22,fullName:"Huawei nova 14",variant:"12Gb/512Gb чёрный EAC",img:"https://img.dns-shop.ru/img/goods/1/1234567911_1.jpg",img2:"https://img.dns-shop.ru/img/goods/1/1234567911_2.jpg",oldPrice:36999,newPrice:33999,specs:["6.7\" OLED","Kirin 8000","60MP","4500 mAh"],brand:"Huawei"},
+    {id:23,fullName:"Huawei nova 14 Pro",variant:"12Gb/512Gb чёрный EAC",img:"https://img.dns-shop.ru/img/goods/1/1234567912_1.jpg",img2:"https://img.dns-shop.ru/img/goods/1/1234567912_2.jpg",oldPrice:46999,newPrice:44999,specs:["6.78\" OLED 120Hz","Kirin 9000","50MP переменная","4500 mAh"],brand:"Huawei"}
 ];
 
 let cart = [];
@@ -105,7 +124,7 @@ function updateCart() {
     `).join('');
 }
 
-// === ОПЛАТА В TON НА ТВОЙ КОШЕЛЁК ===
+// === ОПЛАТА В TON ===
 document.getElementById('checkout-btn').onclick = () => {
     if (cart.length === 0) return tg.showAlert('Корзина пуста!');
 
